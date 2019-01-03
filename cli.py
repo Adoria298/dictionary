07a183978c22939c63e56a8daa86324567a4bc7a
Adoria298 <bananaorange29@outlook.com>
2019-01-03 13:36:53 +0000

    Added cli args

--- cli.py
+++ cli.py
@@ -1,4 +1,14 @@
 from dictionary import Dictionary
+from argparse import ArgumentParser
 
 def parse_search(line: str, dicti: Dictionary):
     return dicti.search(line)
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Dictionary Lookup")
+    parser.add_argument('--dictionary', '-d',   # flags 
+                        dest='dictionary', 
+                        action='store',
+                        help='Path to Dictionary-compatible csv file.')
+    args = parser.parse_args()
+    print(args)
