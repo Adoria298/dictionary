3b6d63e87ce6c29e54189d4be098898299dbe456
Adoria298 <bananaorange29@outlook.com>
2019-01-04 16:05:19 +0000

    Catch the KeyboardInterrupt!

--- cli.py
+++ cli.py
@@ -3,9 +3,12 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(description="Dictionary Lookup")
 parser.add_argument('dictionary',   # positional 
-                        action='store',
-                        help='Path to Dictionary-compatible csv file.')
+                    action='store',
+                    help='Path to Dictionary-compatible csv file.')
 args = parser.parse_args()
 dictionary = Dictionary(args.dictionary)
 while True:
-    print(dictionary.formatted_search(input("> "))
+    try:
+        print(dictionary.formatted_search(input("> ")))
+    except KeyboardInterrupt:
+        quit(1)
