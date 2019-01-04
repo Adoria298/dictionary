00ed7af60b54bb0eea77c37d52853175ba500b4e
Adoria298 <bananaorange29@outlook.com>
2019-01-04 16:00:36 +0000

    Move cli.parse_search to class Dictionary as Dictionary.formatted_search

--- cli.py
+++ cli.py
@@ -1,30 +1,11 @@
 from dictionary import Dictionary
 from argparse import ArgumentParser
 
-def parse_search(line: str, dicti: Dictionary):
-    results = dicti.search(line)
-    if len(results) <= 0:
-        result_str = "No results found for search: " + line
-    else:
-        if len(results) == 1: # not elif so the formatting can be easily changed
-            result_str = "1 result found."
-        else:
-            result_str = str(len(results)) + " results found."
-        for i in results:
-            result_str += f"""
-
-{i['word']}
-{i['type']} - {i['data']}
-{i['defi']}
-            """
-    return result_str
-
-if __name__ == "__main__":
-    parser = ArgumentParser(description="Dictionary Lookup")
-    parser.add_argument('dictionary',   # positional 
+parser = ArgumentParser(description="Dictionary Lookup")
+parser.add_argument('dictionary',   # positional 
                         action='store',
                         help='Path to Dictionary-compatible csv file.')
-    args = parser.parse_args()
-    dictionary = Dictionary(args.dictionary)
-    while True:
-        print(parse_search(input("> "), dicti = dictionary))
+args = parser.parse_args()
+dictionary = Dictionary(args.dictionary)
+while True:
+    print(dictionary.formatted_search(input("> "))
