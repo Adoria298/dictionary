7c9ad90270dca31c0e302dd6c58b029e4b5fdd84
Adoria298 <bananaorange29@outlook.com>
2019-01-04 10:44:16 +0000

    Added basic fomatting

--- cli.py
+++ cli.py
@@ -2,7 +2,22 @@ from dictionary import Dictionary
 from argparse import ArgumentParser
 
 def parse_search(line: str, dicti: Dictionary):
-    return dicti.search(line)
+    results = dicti.search(line)
+    if len(results) < 0:
+        result_str = "No results found for search: " + line
+    else:
+        if len(results) == 1: # not elif so the formatting can be easily changed
+            result_str = "1 result found."
+        else:
+            result_str = str(len(results)) + " results found."
+        for i in results:
+            result_str += f"""
+
+{i['word']}
+{i['type']} - {i['data']}
+{i['defi']}
+            """
+    return result_str
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Dictionary Lookup")
